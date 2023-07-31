0415b6d2410b0c2f0d889bcf66b94e62b99724dd
문지은 <mjieun0956@gmail.com>
2023-07-31 17:42:57 +0900

    <FEAT> 허브 리스트, 리모컨 리스트 페이지 구현
    - 임시 데이터(db.json) 생성
    - Card 컴포넌트 생성
    - pages 디렉토리 정리
    - Card body 내용 다시 정렬 필요함
    - json-server 설치

--- front-end/no-matter/src/App.js
+++ front-end/no-matter/src/App.js
@@ -27,7 +27,7 @@ function App() {
               })}
             </Routes>
           </div>
-          <NavBar/>
+          <NavBar />
         </BrowserRouter>
       )
     }
---
+++ front-end/no-matter/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card({children}) {
+  const cardStyle = {
+    height: '80px',
+  };
+
+  return (
+    <div className="card mb-3" style={cardStyle}>
+        <div className="card-body">
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Card
