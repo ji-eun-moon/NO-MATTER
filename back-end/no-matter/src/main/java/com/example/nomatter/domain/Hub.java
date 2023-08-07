44d8db17372554a24c6b929d16cb47cbdd52e1fc
이도하 <victoryddh5@naver.com>
2023-08-07 17:26:43 +0900

    <FEAT> 초대 관련 코드 만들고 입장까지 완료

--- back-end/no-matter/src/main/java/com/example/nomatter/domain/Hub.java
+++ back-end/no-matter/src/main/java/com/example/nomatter/domain/Hub.java
@@ -1,9 +1,6 @@
 package com.example.nomatter.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Table(name = "hubs")
 public class Hub {
 
--- back-end/no-matter/src/main/java/com/example/nomatter/exception/Errorcode.java
+++ back-end/no-matter/src/main/java/com/example/nomatter/exception/Errorcode.java
@@ -12,7 +12,9 @@ public enum Errorcode {
     INVALID_ID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
     UUID_DUPLICATED(HttpStatus.CONFLICT, ""),
     USER_HUB_NOW_FOUND(HttpStatus.NOT_FOUND, ""),
-    USER_NOT_GRADE(HttpStatus.UNAUTHORIZED, "")
+    USER_NOT_GRADE(HttpStatus.UNAUTHORIZED, ""),
+    INVITE_CODE_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    EXPIRED_INVITE_CODE(HttpStatus.UNAUTHORIZED, "")
     ;
 
     private HttpStatus httpStatus;
