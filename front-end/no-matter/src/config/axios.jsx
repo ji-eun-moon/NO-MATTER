7fe425b28c8f64090547e2e3d6c96f78671050f8
최찬석 <ckstjr3592@naver.com>
2023-08-14 21:04:42 +0900

    <FEAT> 허브 모드변경 명령어 추가 - 허브 수신, 송신 모드변경 명령어 추가 - 메인페이지의 각 허브별 리모컨 사용페이지로 연결 - 불필요한 콘솔로그 주석처리

--- front-end/no-matter/src/config/axios.jsx
+++ front-end/no-matter/src/config/axios.jsx
@@ -1,7 +1,6 @@
 // axios.js
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom'
-
 // 인스턴스 생성
 const instance = axios.create({
   baseURL: 'https://i9c105.p.ssafy.io/api/v1', // 원하는 API 서버의 기본 URL을 설정합니다.
@@ -30,6 +29,7 @@ instance.interceptors.response.use(
     // 응답을 받은 후 처리해야 할 작업을 수행
     return response;
   }, (error) => {
+    
     const originalRequest = error.config;
     // 토큰이 만료되었거나 유효하지 않은 경우에만 토큰을 재발급 받을 수 있도록 조건을 설정
     if (error.response.status === 403 && !originalRequest._retry) {
@@ -37,19 +37,20 @@ instance.interceptors.response.use(
       originalRequest._retry = true;
       // 여기서 토큰을 재발급 받는 작업을 수행
       return axios.post('https://i9c105.p.ssafy.io/api/v1/user/refreshToken', { refreshToken: localStorage.getItem('refreshToken') })
-        .then((response) => {
-          const newAuthToken = response.data[0];
-          sessionStorage.setItem('authToken', response.data[0]);
-          localStorage.setItem('refreshToken', response.data[1]);
-          // 기존 요청에 새로 발급된 토큰을 추가하여 다시 요청
-          originalRequest.headers.Authorization = `Bearer ${newAuthToken}`;
-          return axios(originalRequest);
-        })
-        .catch((error) => {
-          // 토큰 재발급에 실패한 경우 로그인 페이지로 이동하거나 다른 작업을 수행
-          // 여기에 오류 처리 코드를 작성
-          console.log(error)
+      .then((response) => {
+        const newAuthToken = response.data[0];
+        sessionStorage.setItem('authToken', response.data[0]);
+        localStorage.setItem('refreshToken', response.data[1]);
+        // 기존 요청에 새로 발급된 토큰을 추가하여 다시 요청
+        originalRequest.headers.Authorization = `Bearer ${newAuthToken}`;
+        return axios(originalRequest);
+      })
+      .catch((error) => {
+        // 토큰 재발급에 실패한 경우 로그인 페이지로 이동하거나 다른 작업을 수행
+        // 여기에 오류 처리 코드를 작성
           const navigate = useNavigate()
+          localStorage.clear()
+          console.log(error)
           navigate('/login')
           return Promise.reject(error);
         });
--- front-end/no-matter/src/firebase-messaging-sw.js
+++ front-end/no-matter/src/firebase-messaging-sw.js
@@ -35,8 +35,8 @@ async function requestPermission() {
     vapidKey: process.env.REACT_APP_VAPID_KEY,
   });
 
-  if (token) console.log("token: ", token);
-  else console.log("Can not get Token");
+  // if (token) console.log("token: ", token);
+  // else console.log("Can not get Token");
 
   onMessage(messaging, (payload) => {
     console.log("메시지가 도착했습니다.", payload);
